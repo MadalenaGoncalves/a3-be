103597074677754b610dd2fe3d677253c4b9c3c1
madpg <madalena.pg@gmail.com>
2018-03-18 14:11:57 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const port = process.env.PORT || 8081;
+
+const app = express();
+
+// Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+const router = require('./router');
+app.use('/', router);
+app.use(function (req, res, next) {
+	console.log('404 - Client tried to get [' + req.url + ']');
+	res.status(404).send('404 - Sorry cant find that!');
+});
+
+
+app.listen(port);
+console.log('Server running on port ', port);
