a8e857a9ee23db0809ae35512781e813423d0d82
madpg <madalena.pg@gmail.com>
2019-11-15 07:24:22 +0100

    Updates for using heroku

--- src/connection.js
+++ src/connection.js
@@ -9,6 +9,13 @@ const options = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
 };
+
+if (process.env.JAWSDB_URL) {
+  console.log("Connection to JawsDB");
+  options = process.env.JAWSDB_URL;
+} else {
+  console.log("Using local DB");
+}
 const connection = mysql.createConnection(options);
 
 connection.connect(function(err) {
--- src/server.js
+++ src/server.js
@@ -17,12 +17,10 @@ if (process.env.NODE_ENV !== 'production') {
 	}
 }
 
-const port = process.env.PORT || 8888;
-
 const app = express();
 
 // Middleware
-app.use(morgan('dev'));
+app.use(morgan('dev')); // logging middleware: https://github.com/expressjs/morgan
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -50,5 +48,6 @@ app.use(function (req, res, next) {
   res.status(404).send('Not found');
 });
 
+const port = process.env.PORT || 8888;
 app.listen(port);
 console.log('Server running on port ', port);
