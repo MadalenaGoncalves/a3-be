8758aada9784aa4d49cab2e8d3d469c8aec0d8f6
madpg <madalena.pg@gmail.com>
2020-01-17 10:14:16 +0100

    API refactoring: clean-up router

--- src/router.js
+++ src/router.js
@@ -1,90 +1,120 @@
 'use strict';
 
 import express from 'express';
-import path from 'path';
-import api from './api';
+import * as api from './api';
 
 const router = express.Router();
 
 function handleResponse(res, result) {
-  console.log("handling response");
   res.setHeader('Content-Type', 'application/json');
   res.status(result.status);
   res.send(result);
   // TODO check if res.json is better than res.send
 }
 
-
 router.get('/projects', async function(req, res) {
-  const result = await api.projects.getAll();
+  const result = await api.getAllProjectsWithThumbnails();
   handleResponse(res, result);
 });
 
 router.get('/projects/:id', async function(req, res) {
-  const project = await api.projects.getOne(req.params.id);
-  const images = await api.images.queryAllByProjectId(req.params.id);
-  if (!!images) {
-    // handle errors
-    project.data[0].relations = {};
-    project.data[0].relations.images = images;  
-  };
-
-  handleResponse(res, project);
-});
-
-router.get('/projects/:id/images', async function(req, res) {
-  const result = await api.images.getAllByProjectId(req.params.id);
-  handleResponse(res, result);
-});
-
-router.get('/images/:id', async function(req, res) {
-  const result = await api.images.getOne(req.params.id);
+  const result = await api.getOneProjectWithRelations(req);
   handleResponse(res, result);
 });
 
 router.get('/contacts', async function(req, res) {
-  const result = await api.contacts.getAll();
+  const result = await api.getAllContactsWithPhoto();
   handleResponse(res, result);
 });
 
 router.get('/contacts/:id', async function(req, res) {
-  const result = await api.contacts.getOne(req.params.id);
+  const result = await api.getOneContactWithPhoto(req);
   handleResponse(res, result);
 });
 
-// POST / PATCH
+
+// POST
 router.post('/projects', async function(req, res) {
-  const result = await api.projects.create(req.body);
-  handleResponse(res, result);
-});
-
-router.patch('/projects/:id', async function(req, res) {
-  console.log('@patch project', req.params.id, req.body);
-  const result = await api.projects.update(req.params.id, req.body);
-  console.log('@patch after', result);
-  handleResponse(res, result);
-});
-
-router.post('/images', async function(req, res) {
-  const result = await api.images.create(req.body);
-  handleResponse(res, result);
-});
-
-router.patch('/images/:id', async function(req, res) {
-  const result = await api.images.update(req.params.id, req.body);
+  // handle insert to filesystem
+  const result = await api.createProject(req);
   handleResponse(res, result);
 });
 
 router.post('/contacts', async function(req, res) {
-  const result = await api.contacts.create(req.body);
+  // handle insert to filesystem
+  const result = await api.createContact(req);
   handleResponse(res, result);
 });
+
+
+// // PATCH
+// -- aqui tenho de validar primeiro se tenho dados para a imagem e fazer qq coisa com isso
+// router.patch('/projects/:id', async function(req, res) {
+//   console.log('@patch project', req.params.id, req.body);
+//   const result = await api.projects.update(req.params.id, req.body);
+//   console.log('@patch after', result);
+//   handleResponse(res, result);
+// });
 
 router.patch('/contacts/:id', async function(req, res) {
-  const result = await api.contacts.update(req.params.id, req.body);
+  const result = await api.updateContact(req);
   handleResponse(res, result);
 });
 
+// router.patch('/images/:id', async function(req, res) {
+//   const result = await api.images.update(req.params.id, req.body);
+//   handleResponse(res, result);
+// });
+
+
+// DELETE
+router.delete('/projects/:id', async function(req, res) {
+  // handle delete from filesystem
+  const result = await api.deleteProject(req);
+  handleResponse(res, result);
+});
+
+router.delete('/contacts/:id', async function(req, res) {
+  // handle delete from filesystem
+  const result = await api.deleteContact(req);
+  handleResponse(res, result);
+});
+
+router.post('/test/fileupload', async function(req, res) {
+  // handle delete from filesystem
+  await api.fileUpload(req);
+  res.status(200);
+  res.send("ok");
+});
+router.get('/test/filedelete', async function(req, res) {
+  // handle delete from filesystem
+  await api.fileDelete(req);
+  res.status(200);
+  res.send("ok");
+});
+
+// // AUTH
+// router.post('/login', async function(req, res) {
+//   onst result = await api.authenticate(req.params.user, req.params.pass);
+//   res.send('post /login');
+// });
+
+// router.get('/logout', async function(req, res) {
+//   const result = await api.logout();
+//   res.redirect('/');
+//   res.send('get /logout');
+// });
 
 export default router;
 
+// router.get('/all', async function(req, res) {
+//   let projects;
+//   let contacts;
+//   await Promise.all([
+//     api.projects.getAll().then(
+//       result => Promise.resolve(projects = result.data )),
+//     api.contacts.getAll().then(
+//       result => Promise.resolve(contacts = result.data )),
+//   ]);
+//   const result = await api.categories.getAll();
+// });
